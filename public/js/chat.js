88f29cfd08072ee6588bb0ab8325dfcf6f34378c
Sagar Lohana <sagar_lohana@hotmail.com>
2019-03-24 17:07:12 -0400

    Initial commit

---
+++ public/js/chat.js
@@ -0,0 +1,21 @@
+const socket = io()
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const message = e.target.elements.message.value
+    socket.emit('sendMessage', message)
+})
+
+document.querySelector('#send-location').addEventListener('click', (e) => {
+    if(!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position)
+    })
+})
---
+++ src/index.js
@@ -0,0 +1,41 @@
+
+const express = require('express')
+const app = express()
+const http = require('http')
+const socketio = require('socket.io')
+
+const path = require('path')
+const server = http.createServer(app)
+const io = socketio(server)
+
+const publicDirectorypath = path.join(__dirname, '../public')
+const port = process.env.PORT || 3000
+
+app.use(express.static(publicDirectorypath))
+
+let count = 0
+
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
+
+    // Welcome message
+    socket.emit('message', 'Welcome!')
+    
+    // New user message
+    socket.broadcast.emit('message', 'A new user has joined!')
+
+    // Sending message through form
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+    })
+
+    // Disconnection message
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
+    })
+})
+
+
+server.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
